7a75a347930c22843a4688efc58a62ce7de33e9c
sangwookpark <sangwookp9591@gmail.com>
2025-08-14 16:48:15 +0900

    feat : init Toast stories install

---
+++ src/Toast.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react-vite/*';
+import { Toast, ToastType } from './Toast';
+
+const meta: Meta = {
+    title: 'components/toast',
+    component: Toast,
+};
+
+export default meta;
+
+type StoryToast = StoryObj<ToastType>;
+
+export const Default: StoryToast = {};
--- src/Toast.tsx
+++ src/Toast.tsx
@@ -1,5 +1,15 @@
 export type ToastType = {};
 
 export function Toast({}: ToastType) {
-    return <div>toast</div>;
+    return (
+        <div
+            style={{
+                width: 100,
+                height: 100,
+                background: 'red',
+            }}
+        >
+            toast
+        </div>
+    );
 }
