960014d43d45f943c529fef39c11cba721e65e0d
sangwookpark <sangwookp9591@gmail.com>
2025-08-14 22:58:33 +0900

    chore : tsup 수정

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,3 @@
-// tsup.config.ts
 import { defineConfig } from 'tsup';
 import { vanillaExtractPlugin } from '@vanilla-extract/esbuild-plugin';
 
@@ -6,6 +5,9 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
   external: ['react', 'react-dom'],
   esbuildPlugins: [vanillaExtractPlugin()],
 });
