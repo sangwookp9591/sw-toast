37e27c0d237bd362840f4a306df92469552b43c5
sangwookpark <sangwookp9591@gmail.com>
2025-08-14 17:11:48 +0900

    feat : storybook에 vanilla-extract plugin add

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,18 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-docs",
-    "@storybook/addon-onboarding"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-onboarding'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  async viteFinal(config) {
+    if (config.plugins) {
+      config.plugins.push(vanillaExtractPlugin());
+    }
+    return config;
+  },
 };
 export default config;
