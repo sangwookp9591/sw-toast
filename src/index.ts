17bc414c67fe88ef754e6236ef0bbbfbbd62d554
sangwookpark <sangwookp9591@gmail.com>
2025-08-14 23:17:46 +0900

    chore : update

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+import './components/Toast.css';
+
 export { ToastProvider, useToast } from './context/ToastProvider';
 export { type ToastType } from './types/type';
--- tsup.config.ts
+++ tsup.config.ts
@@ -10,4 +10,16 @@ export default defineConfig({
   clean: true,
   external: ['react', 'react-dom'],
   esbuildPlugins: [vanillaExtractPlugin()],
+  // CSS 파일이 dist 폴더에 생성되도록 보장
+  onSuccess: async () => {
+    const fs = await import('fs');
+    const path = await import('path');
+
+    const files = fs.readdirSync('./');
+    files.forEach((file) => {
+      if (file.endsWith('.css')) {
+        fs.copyFileSync(path.join('./', file), path.join('./dist', file));
+      }
+    });
+  },
 });
