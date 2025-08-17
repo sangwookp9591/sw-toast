cc360d6cb8bc5c8d1c43f4527427895423eb2e80
sangwookpark <sangwookp9591@gmail.com>
2025-08-17 22:36:53 +0900

    feat : storybook 추가

--- src/Toast.stories.tsx
+++ src/Toast.stories.tsx
@@ -19,52 +19,105 @@ const ToastDemo = () => {
   };
 
   return (
-    <div style={{ display: 'flex', gap: 12 }}>
-      <button
-        onClick={() =>
-          showToast({
-            message: 'Success!',
-            type: 'success',
-            onClose: handleClose,
-          })
-        }
-      >
-        Success
-      </button>
-      <button
-        onClick={() =>
-          showToast({
-            message: 'Error!',
-            type: 'error',
-            onClose: handleClose,
-          })
-        }
-      >
-        Error
-      </button>
-      <button
-        onClick={() =>
-          showToast({
-            message: 'Warning!',
-            type: 'warning',
-            onClose: handleClose,
-          })
-        }
-      >
-        Warning
-      </button>
-      <button
-        onClick={() =>
-          showToast({
-            message: 'Info!',
-            type: 'info',
-            onClose: handleClose,
-          })
-        }
-      >
-        Info
-      </button>
-    </div>
+    <>
+      <div style={{ display: 'flex', gap: 12 }}>
+        <button
+          onClick={() =>
+            showToast({
+              message: 'Success!',
+              type: 'success',
+              onClose: handleClose,
+            })
+          }
+        >
+          Success
+        </button>
+        <button
+          onClick={() =>
+            showToast({
+              message: 'Error!',
+              type: 'error',
+              onClose: handleClose,
+            })
+          }
+        >
+          Error
+        </button>
+        <button
+          onClick={() =>
+            showToast({
+              message: 'Warning!',
+              type: 'warning',
+              onClose: handleClose,
+            })
+          }
+        >
+          Warning
+        </button>
+        <button
+          onClick={() =>
+            showToast({
+              message: 'Info!',
+              type: 'info',
+              onClose: handleClose,
+            })
+          }
+        >
+          Info
+        </button>
+      </div>
+      <div style={{ display: 'flex', gap: 12 }}>
+        <button
+          onClick={() =>
+            showToast({
+              message: 'Success!',
+              type: 'success',
+              progressColor: 'red',
+              onClose: handleClose,
+            })
+          }
+        >
+          Success color
+        </button>
+        <button
+          onClick={() =>
+            showToast({
+              message: 'Error!',
+              type: 'error',
+              progressColor: 'blue',
+              onClose: handleClose,
+            })
+          }
+        >
+          Error color
+        </button>
+        <button
+          onClick={() =>
+            showToast({
+              message: 'Warning!',
+              type: 'warning',
+              progressBar: false,
+              onClose: handleClose,
+            })
+          }
+        >
+          Warning !progress
+        </button>
+        <button
+          onClick={() =>
+            showToast({
+              message: 'Info!',
+              type: 'info',
+              progressBar: false,
+              pauseOnHover: false,
+              onClose: handleClose,
+            })
+          }
+        >
+          Info !hover !progress
+        </button>
+      </div>
+    </>
   );
 };
 // ✅ Provider로 감싸서 Storybook에서 렌더링
